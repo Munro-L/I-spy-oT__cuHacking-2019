87088f905af354bd60f701bfbc8c6b8d6b7d1b04
michaelPruss <michael_pruss1@restapi.us-east1-b.c.bold-azimuth-231917.internal>
2019-02-17 15:09:56 +0000

    Added more apis

--- restAPI.py
+++ restAPI.py
@@ -8,11 +8,13 @@ api = Api(app)
 
 fullData = []
 currData = []
+locationData = []
 locked = 0
+alarm = False
 
 class LogTemps(Resource):
     def get(self):
-        return {"data": fullData}
+        return {"data": fullData, "code": 0}
 
     def post(self):
         global locked
@@ -36,15 +38,60 @@ class GetSingleTemp(Resource):
         if locked > 0:
             locked -= 1
             if len(currData) > 0:
-                result = {"data": currData.pop(0)}
+                result = {"data": currData.pop(0), "code": 0}
                 return result
             else:
-                return {"data": fullData}
+                return {"data": fullData, "code": 0}
         else:
-            return {"data": -1}
+            return {"data": [], "code": -1}
+
+class GetLast(Resource):
+    def get(self):
+        if len(fullData) > 0:
+            return {"data": fullData[-1], "code": 0}
+        else :
+            return {"data": [], "code": -1}
+
+class Alarm(Resource):
+    def post(self):
+        global press
+        body = loads(request.data)
+        if body == None:
+            return {'status': 'fail'}
+        press = body.get("press")
+        if press == None:
+            return {'status': 'fail'}
+        if press == 0:
+            alarm = False
+        elif press == 1:
+            alarm = True
+        else:
+            return {'status', 'fail'}
+        return {'status': 'success'}
+    def get(self):
+        return {'alarm': press}
+
+
+class Location(Resource):
+    def post(self):
+        body = loads(request.data)
+        if body == None:
+            return {'status': 'fail'}
+        longitude = body.get('long')
+        latitude = body.get('lat')
+        if longitude == None or latitude == None:
+            return {'status': 'fail'}
+        locationData.append({"lat": latitude, "long": longitude})
+        return {'status', 'success'}
+    def get(self):
+        return {'data': locationData}
+
 
 api.add_resource(LogTemps, '/logTemps')
 api.add_resource(GetSingleTemp, '/logTemps/getOne')
+api.add_resource(GetLast, "/logTemps/getLast")
+api.add_resource(Alarm, '/alarm')
+api.add_resource(Location, '/location')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
