e54001bf7a4e90a73d77122bcaab2c34a14abbba
Munro-L <lindsaymunromail@gmail.com>
2019-02-17 00:41:01 -0500

    Using tee for better file output

--- ProxyServer_HelperScripts/tshark_parse.sh
+++ ProxyServer_HelperScripts/tshark_parse.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for pcap in ~/pcaps/*.pcap; do
-	tshark -r "$pcap" -Y "http" -T fields -e ip.src -e ip.dst -e http.response.code 2>/dev/null
+	tshark -r "$pcap" -Y "http" -T fields -e text -e ip.src -e ip.dst -e http.response.code | tee -a  traffic.log
 done
