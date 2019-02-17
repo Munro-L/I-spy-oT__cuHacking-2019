68a22cd8a2c3efaae865e8a16c248c7c262b6f0b
michaelPruss <michael_pruss1@restapi.us-east1-b.c.bold-azimuth-231917.internal>
2019-02-17 06:54:28 +0000

    Made API better

--- restAPI.py
+++ restAPI.py
@@ -1,18 +1,23 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
+from json import loads
 
 app = Flask(__name__)
 api = Api(app)
 
-data = []
+fullData = []
+currData = []
+locked = 0
 
 class LogTemps(Resource):
     def get(self):
-        return {"data": data}
+        return {"data": fullData}
 
     def post(self):
-        body = request.get_json()
+        global locked
+        body = loads(request.data)
+        #body = request.get_json()
         if body == None:
             return {'status': 'fail'}
         temp = body.get("temp")
@@ -20,17 +25,26 @@ class LogTemps(Resource):
         if temp == None or timeStamp == None:
             return {'status': 'fail'}
         curr = {'temp': temp, 'timeStamp': timeStamp}
-        data.append(curr)
+        fullData.append(curr)
+        currData.append(curr)
+        locked += 1
         return {'status':'success'}
+
 class GetSingleTemp(Resource):
     def get(self):
-        if len(data) > 0:
-            return {"data": data[-1]}
+        global locked
+        if locked > 0:
+            locked -= 1
+            if len(currData) > 0:
+                result = {"data": currData.pop(0)}
+                return result
+            else:
+                return {"data": fullData}
         else:
-            return {"data": data}
+            return {"data": -1}
 
 api.add_resource(LogTemps, '/logTemps')
 api.add_resource(GetSingleTemp, '/logTemps/getOne')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
